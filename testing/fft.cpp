e672c6d370da809f52f263292df8fd22f52f8ce7
mgrabner <mgrabner@5a60a2cd-b312-4170-b7e6-586859ae540e>
2008-10-14 13:43:37 +0000

    performance evaluation

--- testing/fft.cpp
+++ testing/fft.cpp
@@ -18,6 +18,8 @@
   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <sys/time.h>
+
 #include <cstdlib>
 #include <iostream>
 
@@ -26,36 +28,64 @@
 #include <cudatemplates/devicememorylinear.hpp>
 #include <cudatemplates/hostmemoryheap.hpp>
 
+using namespace std;
 
-const float EPSILON = 1e-6;  // error threshold
 
+const int   SIZE    =   1024;  // image size
+const int   COUNT   = 100000;  // number of FFTs to perform
+const float EPSILON =   1e-6;  // error threshold
+
+
+double
+operator-(const struct timeval &t1, const struct timeval &t2)
+{
+  return (t1.tv_sec - t2.tv_sec) + (t1.tv_usec - t2.tv_usec) * 1e-6;
+}
 
 int
 main()
 {
-  const int SIZE = 1024;
   Cuda::HostMemoryHeap1D    <Cuda::FFT::real>    data1_h(SIZE);
   Cuda::DeviceMemoryLinear1D<Cuda::FFT::real>    data1_g(SIZE);
   Cuda::DeviceMemoryLinear1D<Cuda::FFT::complex> data_fft_g(SIZE / 2 + 1);
   Cuda::DeviceMemoryLinear1D<Cuda::FFT::real>    data2_g(SIZE);
   Cuda::HostMemoryHeap1D    <Cuda::FFT::real>    data2_h(SIZE);
 
+  // allocate memory:
   Cuda::FFT::Plan<Cuda::FFT::real, Cuda::FFT::complex, 1> plan_r2c_1d(data1_g.size);
   Cuda::FFT::Plan<Cuda::FFT::complex, Cuda::FFT::real, 1> plan_c2r_1d(data1_g.size);
 
+  // create random data:
   for(int i = SIZE; i--;)
     (data1_h.getBuffer())[i] = rand() / (float)RAND_MAX;
 
+  // copy data to device memory:
   copy(data1_g, data1_h);
-  plan_r2c_1d.exec(data1_g, data_fft_g);
-  plan_c2r_1d.exec(data_fft_g, data2_g);
+
+  // execute FFT and measure performance:
+  struct timeval t1, t2;
+  gettimeofday(&t1, 0);
+
+  for(int i = COUNT; i--;) {
+    plan_r2c_1d.exec(data1_g, data_fft_g);
+    plan_c2r_1d.exec(data_fft_g, data2_g);
+  }
+
+  gettimeofday(&t2, 0);
+  double t = t2 - t1;
+  cout
+    << "total time: " << t << " seconds\n"
+    << "FFTs per second (" << SIZE << 'x' << SIZE << ", forward and inverse): " << (COUNT / t) << endl;
+
+  // copy data to host memory:
   copy(data2_h, data2_g);
 
+  // verify results:
   for(int i = SIZE; i--;) {
     float d = (data2_h.getBuffer())[i] / SIZE - (data1_h.getBuffer())[i];
 
     if(fabs(d) > EPSILON) {
-      std::cerr << "FFT failed\n";
+      cerr << "FFT failed\n";
       return 1;
     }
   }
