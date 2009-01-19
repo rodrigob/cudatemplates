dc25b754c7022f7c7f8c592b5583956825528b43
mgrabner <mgrabner@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-01-19 14:08:44 +0000

    using vertex array

--- testing/buffer_object.cpp
+++ testing/buffer_object.cpp
@@ -40,6 +40,11 @@ using namespace std;
 typedef unsigned char PixelType;
 Cuda::OpenGL::Texture<PixelType, 2> texture;
 
+const int SUBDIV = 8;
+
+GLfloat coords[(SUBDIV + 1) * (SUBDIV + 1) * 3];
+int vertices[SUBDIV * SUBDIV * 4];
+
 
 void
 reshape(int w, int h)
@@ -62,12 +67,9 @@ display()
 
   texture.bind();
 
-  glBegin(GL_POLYGON);
-  glTexCoord2f(0, 1); glVertex3f(-1, -1, 0);
-  glTexCoord2f(1, 1); glVertex3f( 1, -1, 0);
-  glTexCoord2f(1, 0); glVertex3f( 1,  1, 0);
-  glTexCoord2f(0, 0); glVertex3f(-1,  1, 0);
-  glEnd();
+  glTranslatef(-1, 1, 0);
+  glScalef(2, -2, 1);
+  glDrawElements(GL_QUADS, SUBDIV * SUBDIV * 4, GL_UNSIGNED_INT, vertices);
 
   glutSwapBuffers();
   glutPostRedisplay();
@@ -105,6 +107,31 @@ main(int argc, char *argv[])
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    GLfloat *p = coords;
+
+    for(int i = 0; i <= SUBDIV; ++i)
+      for(int j = 0; j <= SUBDIV; ++j) {
+	*(p++) = (float)i / SUBDIV;
+	*(p++) = (float)j / SUBDIV;
+	*(p++) = 0;
+      }
+
+    int *v = vertices;
+
+    for(int i = 0; i < SUBDIV; ++i)
+      for(int j = 0; j < SUBDIV; ++j) {
+	int v0 = i * (SUBDIV + 1) + j;
+	*(v++) = v0;
+	*(v++) = v0 + 1;
+	*(v++) = v0 + SUBDIV + 2;
+	*(v++) = v0 + SUBDIV + 1;
+      }
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+    glVertexPointer(3, GL_FLOAT, 0, coords);
+    glTexCoordPointer(2, GL_FLOAT, 3 * sizeof(coords[0]), coords);
 
 #if 1
     // create buffer object:
