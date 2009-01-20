a3f44aff47e800c6b6da6ee6cc11abba47044b0e
mgrabner <mgrabner@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-01-20 12:57:38 +0000

    obsolete file removed improved documentation upload

--- doc/rsync.sh
+++ doc/rsync.sh
@@ -4,4 +4,8 @@ USER=mgrabner,cudatemplates
 HOST=shell.sourceforge.net
 HTDOCS=/home/groups/c/cu/cudatemplates/htdocs
 
-rsync --verbose --recursive --rsh="ssh -l $USER" html $USER@$HOST:$HTDOCS/doc
+# create:
+#ssh -t $USER@$HOST create
+
+# copy:
+rsync --verbose --recursive --delete --rsh="ssh -l $USER" html $USER@$HOST:$HTDOCS/doc
