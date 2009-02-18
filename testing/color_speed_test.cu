e0ff78ad771f6a39b9232fcffbbab5d464cb5508
m_unger <m_unger@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-02-18 12:23:51 +0000

    fixed bugs from last test version

--- testing/color_speed_test.cu
+++ testing/color_speed_test.cu
@@ -4,12 +4,27 @@
 
 #include <cuda_runtime.h>
 
+#include <sys/time.h>
+
 using namespace std;
 
+#include "color_speed_test_kernels.cu"
 
 /**
 Does a speed test for color images
 */
+
+double getTime()
+{
+  cudaThreadSynchronize();
+  timeval time;
+  gettimeofday(&time, NULL);
+  return time.tv_sec * 1000.0 + time.tv_usec / 1000.0;
+}
+
+  inline unsigned int divUp(unsigned int a, unsigned int b) {
+    return (a % b != 0) ? (a / b + 1) : (a / b);
+  }
 
 int CUDAtestMemLoad(int block_size, int num, int width, int height)
 {
@@ -24,21 +39,21 @@ int CUDAtestMemLoad(int block_size, int num, int width, int height)
   Cuda::DeviceMemoryPitched<float4, 2> interleaved_image_out(interleaved_size);
   Cuda::DeviceMemoryPitched<float, 3> plane_image_in(plane_size);
   Cuda::DeviceMemoryPitched<float, 3> plane_image_out(plane_size);
-  COMMONLIB_CHECK_CUDA_ERROR();
+//   COMMONLIB_CHECK_CUDA_ERROR();
 
   interleaved_image_in.initMem(1);
   interleaved_image_out.initMem(1);
   plane_image_in.initMem(1);
   plane_image_out.initMem(1);
-  COMMONLIB_CHECK_CUDA_ERROR();
+//   COMMONLIB_CHECK_CUDA_ERROR();
 
   // prepare fragmentation for processing
   dim3 dimBlock(block_size, block_size, 1);
-  dim3 dimGrid(CommonLib::divUp(interleaved_size[0], block_size), CommonLib::divUp(interleaved_size[1], block_size), 1);
+  dim3 dimGrid(divUp(interleaved_size[0], block_size), divUp(interleaved_size[1], block_size), 1);
 
   cout << "First Test: float4 interleaved image  -  ";
   cudaThreadSynchronize();
-  double start_time = CommonLib::getTime();
+  double start_time = getTime();
   for (int i=0; i<num; i++)
   {
     transferInterleavedKernel<<<dimGrid, dimBlock>>>( interleaved_image_in.getBuffer(),
@@ -49,12 +64,12 @@ int CUDAtestMemLoad(int block_size, int num, int width, int height)
                                                       cudaThreadSynchronize();
   }
   cudaThreadSynchronize();
-  cout << CommonLib::getTime() - start_time << endl;
-  COMMONLIB_CHECK_CUDA_ERROR();
+  cout << getTime() - start_time << endl;
+//   COMMONLIB_CHECK_CUDA_ERROR();
 
   cout << "Second Test: float 3 plane image  -  ";
   cudaThreadSynchronize();
-  start_time = CommonLib::getTime();
+  start_time = getTime();
   for (int i=0; i<num; i++)
   {
     transferPlaneKernel<<<dimGrid, dimBlock>>>( plane_image_in.getBuffer(),
@@ -66,8 +81,8 @@ int CUDAtestMemLoad(int block_size, int num, int width, int height)
                                                 cudaThreadSynchronize();
   }
   cudaThreadSynchronize();
-  cout << CommonLib::getTime() - start_time << endl;
-  COMMONLIB_CHECK_CUDA_ERROR();
+  cout << getTime() - start_time << endl;
+//   COMMONLIB_CHECK_CUDA_ERROR();
 
 
   cout << endl << endl;
