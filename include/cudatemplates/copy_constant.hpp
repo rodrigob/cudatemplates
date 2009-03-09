32a7422deb8a917f08e11f4064d7852415832c1b
m_unger <m_unger@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-03-09 12:59:57 +0000

    modified copy_constant to work with 3D volumes

--- include/cudatemplates/copy_constant.hpp
+++ include/cudatemplates/copy_constant.hpp
@@ -80,6 +80,18 @@ __global__ void copy_constant_check_kernel(Type1 dst, Type2 val, CUDA_KERNEL_SIZ
     dst.data[x + y * dst.stride[0] + z * dst.stride[1]] = val;
 }
 
+template <class T>
+__global__ void copy_constant_check_kernel_3D( T* dst, T val, int width, int height, int depth,
+                                               int pitch, int pitchPlane, size_t offset_z)
+{
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+  int z = threadIdx.z + offset_z;
+
+  if((x < width) && (y < height) && (z < depth))
+    dst[x + y*pitch + z*pitchPlane] = val;
+}
+
 namespace Cuda {
 
 /**
@@ -114,20 +126,48 @@ void
 copy(DeviceMemory<Type, Dim> &dst, Type val,
      const Size<Dim> &dst_ofs, const Size<Dim> &size)
 {
-  dst.checkBounds(dst_ofs, size);
-  dim3 gridDim, blockDim;
-  bool aligned;
-  size_t dofs;
-  Size<Dim> rmin, rmax;
-  dst.getExecutionConfiguration(gridDim, blockDim, aligned, dofs, rmin, rmax, dst_ofs, size);
-  typename DeviceMemory<Type, Dim>::KernelData kdst(dst);
-  kdst.data += dofs;
+  if (Dim<3)
+  {
+    dst.checkBounds(dst_ofs, size);
+    dim3 gridDim, blockDim;
+    bool aligned;
+    size_t dofs;
+    Size<Dim> rmin, rmax;
+    dst.getExecutionConfiguration(gridDim, blockDim, aligned, dofs, rmin, rmax, dst_ofs, size);
+    typename DeviceMemory<Type, Dim>::KernelData kdst(dst);
+    kdst.data += dofs;
 
-  if(aligned)
-    copy_constant_nocheck_kernel<<<gridDim, blockDim>>>(kdst, val, Dimension<Dim>());
-  else
-    copy_constant_check_kernel<<<gridDim, blockDim>>>(kdst, val, rmin, rmax);
+    if(aligned)
+      copy_constant_nocheck_kernel<<<gridDim, blockDim>>>(kdst, val, Dimension<Dim>());
+    else
+      copy_constant_check_kernel<<<gridDim, blockDim>>>(kdst, val, rmin, rmax);
+  }
+  else if (Dim == 3)
+  {
+    dst.checkBounds(dst_ofs, size);
+    dim3 gridDim_dummy, blockDim_dummy;
+    bool aligned;
+    size_t dofs;
+    Size<Dim> rmin, rmax;
+    dst.getExecutionConfiguration( gridDim_dummy, blockDim_dummy, aligned,
+                                   dofs, rmin, rmax, dst_ofs, size);
 
+    // Compute optimal blocksize
+    unsigned int block_size = 16;
+    unsigned int block_size_z = 2;
+
+    // prepare fragmentation for processing
+    dim3 dimBlock(block_size, block_size, block_size_z);
+    dim3 dimGrid(CommonLib::divUp(rmax[0], block_size), CommonLib::divUp(rmax[1], block_size), 1);
+
+    for (unsigned int offset_z=0; offset_z<rmax[1]; offset_z+=block_size_z)
+    {
+      copy_constant_check_kernel_3D<<<dimGrid, dimBlock>>>( &dst.getBuffer()[dofs], val,
+                                                            rmax[0], rmax[1], rmax[2],
+                                                            dst.stride[0], dst.stride[1],
+                                                            offset_z);
+    }
+  }
   CUDA_CHECK(cudaGetLastError());
 }
 
