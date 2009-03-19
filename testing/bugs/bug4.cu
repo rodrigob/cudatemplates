3ff6d6c3b8a092461e335aab2f0d6029aebe7620
mgrabner <mgrabner@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-03-19 10:20:48 +0000

    more info added

--- testing/bugs/bug4.cu
+++ testing/bugs/bug4.cu
@@ -1,3 +1,26 @@
+/*
+
+Compiling this file with "nvcc --keep bug4.cu" gives the following error message:
+
+In file included from bug4.cu:37:
+bug4.cudafe1.stub.c: In function ‘void __sti____cudaRegisterAll_12_bug4_cpp1_ii_main()’:
+bug4.cudafe1.stub.c:23: error: insufficient contextual information to determine type
+
+The error dos not appear when "bool" is replaced by "int".
+
+system information:
+Linux openSUSE-11.1 x86_64 kernel 2.6.27.19-3.2-default
+
+nvcc version:
+Built on Thu_Mar__5_04:25:57_PST_2009
+Cuda compilation tools, release 2.2, V0.2.1221
+
+gcc version:
+gcc (SUSE Linux) 4.3.2 [gcc-4_3-branch revision 141291]
+
+*/
+
+
 template<class T>
 __global__ void kernel(const T *p, bool x)
 {
