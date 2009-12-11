5dac1e6300862296c3dfe0eb3eb1f083581d4920
mgrabner <mgrabner@5a60a2cd-b312-4170-b7e6-586859ae540e>
2009-12-11 12:05:20 +0000

    workaround and possible explanation

--- testing/bugs/bug9.cpp
+++ testing/bugs/bug9.cpp
@@ -12,8 +12,15 @@ int main() {
   for (size_t i = 0; i < my_size; i++)
 	input[i] = i;  
   
+#if 0
+  // this creates a function pointer of type "Cuda::HostMemoryHeap<int, 1u> (*)(Cuda::Size<1u>)":
   HostMemoryHeap<int,1> host(Size<1>(my_size));
-  
+#else
+  // this creates an object of type "Cuda::HostMemoryHeap<int, 1u>":
+  Size<1> size(my_size);
+  HostMemoryHeap<int,1> host(size);
+#endif
+
   for (size_t i = 0; i < my_size; i++)
     host.getBuffer()[i] = input[i];  
 
